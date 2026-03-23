11ce0f725564f651ff4e43c87f59f0495dcbd72c
labidi-houssem <houssemm.labidi@gmail.com>
2026-03-23 15:57:39 +0100

    Add Gestion formation

---
+++ Gestion-Formation/src/main/java/org/exemple/gestionformation/GestionFormationApplication.java
@@ -0,0 +1,13 @@
+package org.exemple.gestionformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionFormationApplication.class, args);
+    }
+
+}
---
+++ Gestion-Formation/src/test/java/org/exemple/gestionformation/GestionFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exemple.gestionformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
